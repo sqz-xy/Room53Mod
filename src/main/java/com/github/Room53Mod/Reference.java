21299d6f0f10d28d487737a746a66ff0f0e080c2
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-12-05 21:58:55 +0000

    Setup project file structure and base mod files/dependencies

---
+++ src/main/java/com/github/Room53Mod/Reference.java
@@ -0,0 +1,10 @@
+package com.github.Room53Mod;
+
+public class Reference
+{
+    public static final String MODID = "rm";
+    public static final String NAME = "Room53";
+    public static final String VERSION = "1.0";
+    public static final String CLIENT = "Room53.Room53mod.proxy.ClientProxy";
+    public static final String COMMON = "Room53.Room53mod.proxy.CommonProxy";
+}
---
+++ src/main/java/com/github/Room53Mod/Room53Mod.java
@@ -0,0 +1,27 @@
+package com.github.Room53Mod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class Room53Mod
+{
+    @Mod.Instance
+    public static Room53Mod instance;
+
+    private static Logger logger;
+
+    @EventHandler
+    public static void preInit(FMLPreInitializationEvent event) { /* logger = event.getModLog(); */ }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) { /* logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName()); */ }
+
+    @EventHandler
+    public static void postInit(FMLPostInitializationEvent event) { /*logger = event.getModLog(); */ }
+
+}
