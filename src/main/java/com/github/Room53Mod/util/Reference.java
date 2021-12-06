3653432cc69dff50779c41dfd245154d878776dd
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-12-06 15:40:36 +0000

    Fixed Reference

--- src/main/java/com/github/Room53Mod/util/Reference.java
+++ src/main/java/com/github/Room53Mod/util/Reference.java
@@ -5,6 +5,7 @@ public class Reference
     public static final String MODID = "rm53";
     public static final String NAME = "Room53";
     public static final String VERSION = "1.0";
-    public static final String CLIENT = "Room53.Room53mod.proxy.ClientProxy";
-    public static final String COMMON = "Room53.Room53mod.proxy.CommonProxy";
+
+    public static final String CLIENT = "com.github.Room53Mod.proxy.ClientProxy";
+    public static final String COMMON = "com.github.Room53Mod.proxy.CommonProxy";
 }
