ee39dc52d5d555ed68111da308e6a1cd456e2be4
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-12-06 15:31:13 +0000

    Added RegistryHandler.java

--- src/main/java/com/github/Room53Mod/Room53Mod.java
+++ src/main/java/com/github/Room53Mod/Room53Mod.java
@@ -2,14 +2,13 @@ package com.github.Room53Mod;
 
 import com.github.Room53Mod.proxy.CommonProxy;
 import com.github.Room53Mod.util.Reference;
-import net.minecraft.item.Item;
-import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
@@ -22,20 +21,22 @@ public class Room53Mod
     public static CommonProxy proxy;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event){
 
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event){
 
     }
 
     @EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void serverInit(FMLServerStartingEvent event){
 
     }
 }
---
+++ src/main/java/com/github/Room53Mod/util/handlers/RegistryHandler.java
@@ -0,0 +1,52 @@
+package com.github.Room53Mod.util.handlers;
+
+import com.github.Room53Mod.init.ItemInit;
+import com.github.Room53Mod.util.interfaces.IHasModel;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+public class RegistryHandler
+{
+    @SubscribeEvent
+    public static void onItemRegister(RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().registerAll(ItemInit.ITEMS.toArray(new Item[0]));
+    }
+
+    @SubscribeEvent
+    public static void onModelRegister(ModelRegistryEvent event)
+    {
+        for(Item item : ItemInit.ITEMS)
+        {
+            if(item instanceof IHasModel)
+            {
+                ((IHasModel)item).registerModels();
+            }
+        }
+    }
+
+    public static void preInitRegistries()
+    {
+
+    }
+
+    public static void initRegistries()
+    {
+
+    }
+
+    public static void postInitRegistries()
+    {
+
+    }
+
+    public static void serverRegistries(FMLServerStartingEvent event)
+    {
+
+    }
+}
