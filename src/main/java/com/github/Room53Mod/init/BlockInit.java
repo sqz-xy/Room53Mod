67228c09cd2684ba33332e0e8bd4cc2d904b8115
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-12-11 22:16:27 +0000

    Setup BlockBase and BlockInit, including the addition of a test_block

--- src/main/java/com/github/Room53Mod/init/BlockInit.java
+++ src/main/java/com/github/Room53Mod/init/BlockInit.java
@@ -1,4 +1,14 @@
 package com.github.Room53Mod.init;
 
+import com.github.Room53Mod.objects.items.BlockBase;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlockInit {
+    public static final List<Block> BLOCKS = new ArrayList();
+
+    public static final Block TEST_BLOCK = new BlockBase("test_block", Material.IRON);
 }
---
+++ src/main/java/com/github/Room53Mod/objects/items/BlockBase.java
@@ -0,0 +1,31 @@
+package com.github.Room53Mod.objects.items;
+
+import com.github.Room53Mod.Room53Mod;
+import com.github.Room53Mod.init.BlockInit;
+import com.github.Room53Mod.init.ItemInit;
+import com.github.Room53Mod.util.interfaces.IHasModel;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+import java.util.Objects;
+
+public class BlockBase extends Block implements IHasModel {
+    public BlockBase(String pName, Material pMaterial) {
+        super(pMaterial);
+        setUnlocalizedName(pName);
+        setRegistryName(pName);
+        setCreativeTab(CreativeTabs.MATERIALS);
+
+        BlockInit.BLOCKS.add(this);
+        ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(Objects.requireNonNull(this.getRegistryName()))); //Revert this to just this.getRegistryName if it breaks
+    }
+
+    @Override
+    public void registerModels() {
+        Room53Mod.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+    }
+}
