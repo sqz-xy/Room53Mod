8ca679f15bbc16fcd1c2fb4856e745af96c0a3d9
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-12-12 20:27:25 +0000

    Implemented Cumium item, to drop from ore

--- src/main/java/com/github/Room53Mod/init/ItemInit.java
+++ src/main/java/com/github/Room53Mod/init/ItemInit.java
@@ -1,5 +1,6 @@
 package com.github.Room53Mod.init;
 
+import com.github.Room53Mod.objects.items.materials.CumiumGem;
 import com.github.Room53Mod.objects.items.testitems.TestItem;
 import com.github.Room53Mod.objects.items.tools.ToolSword;
 import net.minecraft.item.Item;
@@ -12,13 +13,16 @@ public class ItemInit {
     // Init
     public static final List<Item> ITEMS = new ArrayList<>();
 
-    //Materials
+    // Tool Materials
     public static final Item.ToolMaterial TOOL_PURPLE = EnumHelper.addToolMaterial("tool_purple",4, 1500, 5.0f, 10.0f, 19);
 
     // Items
     public static final Item TEST_ITEM = new TestItem("test_item");
 
-    //Tools - Sword
+    // Tools - Sword
     public static final Item PURPLE_SWORD = new ToolSword("purple_sword", TOOL_PURPLE);
+
+    // Materials
+    public static final Item CUMIUM_GEM = new CumiumGem("cumium_gem");
 
 }
---
+++ src/main/java/com/github/Room53Mod/objects/items/materials/CumiumGem.java
@@ -0,0 +1,11 @@
+package com.github.Room53Mod.objects.items.materials;
+
+import com.github.Room53Mod.Room53Mod;
+import com.github.Room53Mod.objects.items.baseclasses.ItemBase;
+
+public class CumiumGem extends ItemBase {
+    public CumiumGem(String pName) {
+        super(pName);
+        setCreativeTab(Room53Mod.ROOM53MODTAB);
+    }
+}
