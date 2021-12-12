cb41780e67fafecab4d08ade5e0c6a9bc778a94b
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-12-12 21:08:22 +0000

    Cumium ore now drops cumium gems

--- src/main/java/com/github/Room53Mod/objects/blocks/ores/CumiumOre.java
+++ src/main/java/com/github/Room53Mod/objects/blocks/ores/CumiumOre.java
@@ -1,12 +1,28 @@
 package com.github.Room53Mod.objects.blocks.ores;
 
 import com.github.Room53Mod.Room53Mod;
+import com.github.Room53Mod.init.ItemInit;
 import com.github.Room53Mod.objects.blocks.baseclasses.BlockBase;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class CumiumOre extends BlockBase {
     public CumiumOre(String pName, Material pMaterial) {
         super(pName, pMaterial);
-        setCreativeTab(Room53Mod.ROOM53MODTAB);
+        setHardness(4.5f);
+        setHarvestLevel("pickaxe", 3);
     }
+
+    @Override
+    public Item getItemDropped(IBlockState pState, Random pRand, int pFortune) {
+        return ItemInit.CUMIUM_GEM;
+    }
+
+
 }
