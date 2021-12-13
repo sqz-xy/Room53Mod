feb92a90a816ef15d3693bd855672b1a3383795e
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-12-13 11:04:09 +0000

    Added world gen package with WorldGenOres.java

---
+++ src/main/java/com/github/Room53Mod/world/gen/WorldGenOres.java
@@ -0,0 +1,15 @@
+package com.github.Room53Mod.world.gen;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraftforge.fml.common.IWorldGenerator;
+
+import java.util.Random;
+
+public class WorldGenOres implements IWorldGenerator {
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+
+    }
+}
