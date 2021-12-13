7ecadb1f584641161ed2fdb98f57e4f8108e2966
Tom Beet <52246751+sqz-xy@users.noreply.github.com>
2021-12-13 12:44:16 +0000

    Cumium ore is now smeltable

--- src/main/java/com/github/Room53Mod/recipes/SmeltingRecipes.java
+++ src/main/java/com/github/Room53Mod/recipes/SmeltingRecipes.java
@@ -1,4 +1,17 @@
 package com.github.Room53Mod.recipes;
 
+import com.github.Room53Mod.init.BlockInit;
+import com.github.Room53Mod.init.ItemInit;
+import com.github.Room53Mod.objects.blocks.ores.CumiumOre;
+import com.github.Room53Mod.objects.items.materials.CumiumGem;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 public class SmeltingRecipes {
+
+    public static void init() {
+        // Add more smelting recipes here
+        GameRegistry.addSmelting(new ItemStack(BlockInit.CUMIUM_ORE), new ItemStack(ItemInit.CUMIUM_GEM), 0.4f);
+    }
 }
